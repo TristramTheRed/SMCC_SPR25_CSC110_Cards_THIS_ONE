4d2c74bb9fe44bb8b8f411719b917f8f0d1f9384
TristramTheRed <tri2196740@maricopa.edu>
2025-05-06 02:06:34 -0700

    Update App.java
    Added
           //Weell, more like BECAUSE etc etc.

--- Blackjack03-1/App.java
+++ Blackjack03-1/App.java
@@ -1,4 +1,4 @@
-//Tristan Biggs 
+0//Tristan Biggs 
 //for invokeLater
 import javax.swing.SwingUtilities;
 
@@ -7,6 +7,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         SortCards deck = new SortCards();
        //MUST BE INCLUDED TO COMPILE SWING COMPONENTS ON EDT
+       //Weell, more like BECAUSE etc etc. 
+
         SwingUtilities.invokeLater(() -> {
             deck.startGame();
             
